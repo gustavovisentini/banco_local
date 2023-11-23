25163db87a2dd8b14dc6399353dedcd674cdc7c1
Gustavo Visentini <gustavo.visentini@gmail.com>
2023-11-23 20:42:55 -0300

    update view

---
+++ lib/homeApp.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:intl/date_symbol_data_local.dart';
+
+class HomeApp extends StatefulWidget {
+  const HomeApp({super.key});
+
+  @override
+  State<HomeApp> createState() => _HomeAppState();
+}
+
+class _HomeAppState extends State<HomeApp> {
+  TextEditingController _tituloController = TextEditingController();
+  TextEditingController _descricaoController = TextEditingController();
+
+  _exibirTelaCadastro() {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text(" anotação"),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                TextField(
+                  controller: _tituloController,
+                  autofocus: true,
+                  decoration: InputDecoration(
+                      labelText: "Título", hintText: "Digite título..."),
+                ),
+                TextField(
+                  controller: _descricaoController,
+                  decoration: InputDecoration(
+                      labelText: "Descrição", hintText: "Digite descrição..."),
+                )
+              ],
+            ),
+            actions: <Widget>[
+              TextButton(
+                  onPressed: () => Navigator.pop(context),
+                  child: Text("Cancelar")),
+              TextButton(
+                  onPressed: () {
+                    //salvar
+                    // _salvarAtualizarAnotacao(anotacaoSelecionada: anotacao);
+
+                    Navigator.pop(context);
+                  },
+                  child: Text('textoSalvarAtualizar'))
+            ],
+          );
+        });
+  }
+
+  _recuperarAnotacoes() async {}
+
+  _salvarAtualizarAnotacao() async {}
+
+  _formatarData(String data) {
+    initializeDateFormatting("pt_BR");
+
+    //Year -> y month-> M Day -> d
+    // Hour -> H minute -> m second -> s
+    //var formatador = DateFormat("d/MMMM/y H:m:s");
+    var formatador = DateFormat.yMd("pt_BR");
+
+    DateTime dataConvertida = DateTime.parse(data);
+    String dataFormatada = formatador.format(dataConvertida);
+
+    return dataFormatada;
+  }
+
+  _removerAnotacao(int id) async {}
+
+  @override
+  void initState() {
+    super.initState();
+    _recuperarAnotacoes();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Minhas anotações"),
+        backgroundColor: Colors.lightGreen,
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+              child: ListView.builder(
+                  itemCount: 0,
+                  itemBuilder: (context, index) {
+                    final anotacao = 0;
+
+                    return Card(
+                      child: ListTile(
+                        title: Text(''),
+                        subtitle: Text(''),
+                        trailing: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                            GestureDetector(
+                              onTap: () {
+                                _exibirTelaCadastro();
+                              },
+                              child: Padding(
+                                padding: EdgeInsets.only(right: 16),
+                                child: Icon(
+                                  Icons.edit,
+                                  color: Colors.green,
+                                ),
+                              ),
+                            ),
+                            GestureDetector(
+                              onTap: () {
+                                //_removerAnotacao(anotacao.id);
+                              },
+                              child: Padding(
+                                padding: EdgeInsets.only(right: 0),
+                                child: Icon(
+                                  Icons.remove_circle,
+                                  color: Colors.red,
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    );
+                  }))
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.green,
+          foregroundColor: Colors.white,
+          child: Icon(Icons.add),
+          onPressed: () {
+            _exibirTelaCadastro();
+          }),
+    );
+  }
+}
